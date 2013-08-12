2cff68c6521a3531f9f79ba6605d5adfbfc4259f
Nobuhiro Nikushi <deneb.ge@gmail.com>
2013-08-12 15:58:40 +0900

    concurrency by EM::Iterator

--- bin/do_get.rb
+++ bin/do_get.rb
@@ -7,6 +7,7 @@ argv = ARGV.dup
 slop = Slop.new(:strict => true, :help => true)
 slop.banner '$ do_get.rb [options]'
 slop.on :t, :timeout=, 'timeout in second (default: 10s)', :default => 10
+slop.on :c, :concurrency=, 'concurrent connection', :default => 64
 slop.on :i, :inputfile=, 'url list file'
 slop.on :n, 'dry-run mode. just print out the result, do not post data to GrowthForecast'
 slop.on :d, 'debug', 'debug output'
@@ -21,20 +22,23 @@ $opts = slop.to_hash
 $opts.delete(:help)
 
 uris = open($opts[:inputfile]).readlines.map{|uri| uri.chomp }
-pending = uris.size
 
 EM.run do
-  uris.each do |uri|
-    http = EM::HttpRequest.new(uri, :connect_timeout => $opts[:timeout], :inactivity_timeout => $opts[:timeout]).get
-    http.callback do
-      puts "#{uri} #{http.response_header.status}"
-      pending -= 1
-      EM.stop if pending < 1
-    end
-    http.errback do
-      puts "#{uri} #{http.error}"
-      pending -= 1
-      EM.stop if pending < 1
-    end
-  end
+  EM::Iterator.new(uris, $opts[:conncurrency]).each(
+    proc {|uri, iter|
+      http = EM::HttpRequest.new(uri, :connect_timeout => $opts[:timeout], :inactivity_timeout => $opts[:timeout]).get
+      http.callback do
+        puts "#{uri} #{http.response_header.status}"
+        iter.next
+      end
+      http.errback do
+        puts "#{uri} #{http.error}"
+        iter.next
+      end
+    },
+    proc {
+      puts "All done!"
+      EM.stop
+    }
+  )
 end
